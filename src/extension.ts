a28e8de4d985d472cd226b3d6d4319ed3b0c38be
al <usernamehw@gmail.com>
2022-10-25 21:53:32 +0300

    feat: highlight multi-cursor (#7)

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,16 @@
-import vscode, { Disposable, TextEditor, window, workspace } from 'vscode';
+import vscode, { DecorationRenderOptions, Disposable, Range, Selection, TextEditor, window, workspace } from 'vscode';
+
+const decorationOptions: DecorationRenderOptions = {
+	isWholeLine: true,
+	backgroundColor: new vscode.ThemeColor('highlightLogicalLine.background'),
+};
+let decorationType: vscode.TextEditorDecorationType;
+/**
+ * Keep last decoration line numbers (to not update decorations when cursor moves (often)).
+ */
+let lastCursorLines: Set<number> = new Set();
 
 export function activate() {
-	const decorationOptions = {
-		isWholeLine: true,
-		backgroundColor: new vscode.ThemeColor('highlightLogicalLine.background'),
-	};
-	let decorationType: vscode.TextEditorDecorationType;
-	let activeEditor = window.activeTextEditor;
-	let lastPositionLine = 999999;
 	let selectionChangeDisposable: Disposable | undefined;
 	let isActive = false;
 
@@ -19,44 +22,25 @@ export function activate() {
 		}
 	});
 	window.onDidChangeActiveTextEditor(textEditor => {
-		lastPositionLine = 999999;
-
 		wordWrapCheck(textEditor);
-		activeEditor = textEditor;
-
-		if (!activeEditor) {
-			return;
-		}
-
-		lastPositionLine = activeEditor.selection.active.line;
 	});
 
 	function updateAllEditorDecorations() {
-		window.visibleTextEditors.forEach(editor => {
-			const activePosition = editor.selection.active;
-			editor.setDecorations(decorationType, [{
-				range: new vscode.Range(activePosition, activePosition),
-			}]);
-		});
+		for (const editor of window.visibleTextEditors) {
+			updateDecorationsForEditor(editor);
+		}
 	}
 
 	function onDidChangeTextEditorSelection(e: vscode.TextEditorSelectionChangeEvent) {
-		if (!activeEditor) {
-			return;
-		}
-		const activePosition = activeEditor.selection.active;
+		const newCursorLines = selectionsToCursorLineNumbers(e.textEditor.selections);
 
-		if (lastPositionLine !== activePosition.line) {
-			activeEditor.setDecorations(decorationType, [{
-				range: new vscode.Range(activePosition, activePosition),
-			}]);
+		if (!isSetTheSame(lastCursorLines, newCursorLines)) {
+			updateDecorationsForEditor(e.textEditor);
 		}
-
-		lastPositionLine = activePosition.line;
 	}
 
 	/**
-	 * Update events listeners and decorations in 2 cases:
+	 * Update events listeners and decorations in 2 additional cases (on top of main case when editor selection changes):
 	 * - Setting 'editor.wordWrap' changed
 	 * - Active text editor/tab changed
 	 */
@@ -67,11 +51,10 @@ export function activate() {
 			disposeAll([selectionChangeDisposable, decorationType]);
 			isActive = false;
 		} else {
-			if (isActive) {
-				return;
+			if (!isActive) {
+				decorationType = window.createTextEditorDecorationType(decorationOptions);
+				selectionChangeDisposable = window.onDidChangeTextEditorSelection(onDidChangeTextEditorSelection);
 			}
-			decorationType = window.createTextEditorDecorationType(decorationOptions);
-			selectionChangeDisposable = window.onDidChangeTextEditorSelection(onDidChangeTextEditorSelection);
 
 			updateAllEditorDecorations();
 			isActive = true;
@@ -79,11 +62,34 @@ export function activate() {
 	}
 }
 
+function updateDecorationsForEditor(editor: TextEditor) {
+	// When at least 1 selection (not empty selection) - do not show any decorations (same as VSCode)
+	if (editor.selections.some(selection => !selection.isEmpty)) {
+		lastCursorLines = new Set();
+		editor.setDecorations(decorationType, []);
+		return;
+	}
+
+	lastCursorLines = selectionsToCursorLineNumbers(editor.selections);
+
+	editor.setDecorations(decorationType, Array.from(lastCursorLines, (lineNumber => new Range(lineNumber, 0, lineNumber, 0))));
+}
+
+function selectionsToCursorLineNumbers(selections: Selection[]): Set<number> {
+	return new Set(selections.filter(selection => selection.isEmpty)
+		.map(selection => selection.start.line));
+}
+
+/**
+ * Return true if 2 sets are identical.
+ */
+function isSetTheSame(set1: Set<number>, set2: Set<number>): boolean {
+	return set1.size === set2.size && Array.from(set1).every(lineNumber => set2.has(lineNumber));
+}
+
 function disposeAll(disposables: (Disposable | undefined)[]) {
 	for (const disposable of disposables) {
-		if (disposable) {
-			disposable.dispose();
-		}
+		disposable?.dispose();
 	}
 }
 
